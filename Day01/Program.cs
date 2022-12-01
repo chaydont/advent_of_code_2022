470a5262f149e9e06b26baa2b417f55544da20fc
Célian Haydont <celian.haydont@dataiku.com>
2022-12-01 06:48:25 +0100

    Day 01

---
+++ Day01/Program.cs
@@ -0,0 +1,18 @@
+﻿using StreamReader file = new("input.txt");
+
+List<int> elves = new();
+int current = 0;
+while (file.ReadLine() is { } ln) {
+    if (ln == "") {
+        elves.Add(current);
+        current = 0;
+    } else {
+        current += int.Parse(ln);
+    }
+}  
+file.Close();
+
+elves.Sort();
+
+Console.WriteLine($"Part 1: {elves.Last()}");
+Console.WriteLine($"Part 2: {elves.TakeLast(3).Sum()}");
