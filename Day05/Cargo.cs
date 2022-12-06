be5c79b4e5417055905b316cb1ec90cb6eb50dbe
Célian <celianhaydont@gmail.com>
2022-12-06 20:07:41 +0100

    Day 05

---
+++ Day05/Cargo.cs
@@ -0,0 +1,80 @@
+﻿using System.Text;
+
+namespace Day05; 
+
+public class Cargo {
+
+    public class Action {
+
+        public int start;
+        public int end;
+        public int n;
+        
+        public Action(int n, int start, int end) {
+            this.n = n;
+            this.start = start;
+            this.end = end;
+        }
+    }
+
+    private readonly List<Stack<char>> crates;
+
+    public Cargo(List<LinkedList<char>> crates) {
+        this.crates = new List<Stack<char>>();
+        foreach (LinkedList<char> list in crates) {
+            this.crates.Add(new Stack<char>(list));
+        }
+    }
+
+    public void ExecuteAction9000(Action action) {
+        for (int i = 0; i < action.n; i++) {
+            MoveCrate(action.start, action.end);
+        }
+    }
+
+    private void MoveCrate(int from, int to) {
+        char crate = crates[from - 1].Pop();
+        crates[to - 1].Push(crate);
+    }
+    
+    public void ExecuteAction9001(Action action) {
+        Stack<char> removedCrates = new();
+        for (int i = 0; i < action.n; i++) {
+            removedCrates.Push(crates[action.start - 1].Pop());
+        }
+
+        foreach (char crate in removedCrates) {
+            crates[action.end - 1].Push(crate);
+        }
+    }
+
+    public string GetTopCrates() {
+        StringBuilder builder = new();
+        foreach (Stack<char> stack in crates) {
+            builder.Append(stack.Pop());
+        }
+        return builder.ToString();
+    }
+
+    public override string ToString() {
+        StringBuilder builder = new();
+        for (int h = crates.Select(stack => stack.Count).Max() - 1; h >= 0; h--) {
+            foreach (List<char> stack in crates.Select(t => t.ToList())) {
+                stack.Reverse();
+                if (stack.Count > h) {
+                    builder.Append($"[{stack[h]}] ");
+                } else {
+                    builder.Append("    ");
+                }
+            }
+
+            builder.Append('\n');
+        }
+
+        for (int i = 1; i <= crates.Count; i++) {
+            builder.Append($" {i}  ");
+        }
+
+        return builder.ToString();
+    }
+}
---
+++ Day05/Solver.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Day05;
+
+public class Solver {
+    public string Part1 { get; }
+    public string Part2 { get; }
+
+    public Solver(string input) {
+        List<LinkedList<char>> crates = new();
+        List<Cargo.Action> actions = new();
+        using (StreamReader file = new(input)) {
+            int length = -1;
+            bool initialized = false;
+            while (file.ReadLine() is { } line) {
+                if (length < 0) {
+                    length = (line.Length + 1) / 4;
+                    for (int i = 0; i < length; i++) {
+                        crates.Add(new LinkedList<char>());
+                    }
+                }
+
+                if (!initialized) {
+                    for (int i = 0; i < length; i++) {
+                        char crate = line[1 + i * 4];
+                        if (crate == '1') {
+                            initialized = true;
+                            break;
+                        } else if (crate != ' ') {
+                            crates[i].AddFirst(crate);
+                        }
+                    }
+                } else {
+                    Regex regex = new("move ([\\d]+) from ([\\d]+) to ([\\d]+)");
+                    if (regex.IsMatch(line)) {
+                        GroupCollection groups = regex.Match(line).Groups;
+                        actions.Add(new Cargo.Action(
+                            int.Parse(groups[1].Value), 
+                            int.Parse(groups[2].Value), 
+                            int.Parse(groups[3].Value
+                            )));
+                    }
+                }
+            }
+        }
+
+        Cargo cargo9000 = new(crates);
+        Cargo cargo9001 = new(crates);
+        
+        foreach (Cargo.Action action in actions) {
+            cargo9000.ExecuteAction9000(action);
+            cargo9001.ExecuteAction9001(action);
+        }
+        Part1 = cargo9000.GetTopCrates();
+        Part2 = cargo9001.GetTopCrates();
+    }
+}
