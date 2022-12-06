84f09130cdd9f247a3213b06dab1f2e54fb62f10
Célian <celianhaydont@gmail.com>
2022-12-06 18:12:54 +0100

    Day 04

---
+++ Day04/Solver.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Day04;
+
+public class Solver {
+    public int Part1 { get; }
+    public int Part2 { get; }
+
+    public Solver(string input) {
+        using StreamReader file = new(input);
+        while (file.ReadLine() is { } line) {
+            Regex regex = new("([\\d]+)-([\\d]+),([\\d]+)-([\\d]+)");
+            Match match = regex.Match(line);
+            int a1 = int.Parse(match.Groups[1].Value);
+            int a2 = int.Parse(match.Groups[2].Value);
+            int b1 = int.Parse(match.Groups[3].Value);
+            int b2 = int.Parse(match.Groups[4].Value);
+            if (a1 >= b1 && a2 <= b2) {
+                Part1++;
+            } else if (b1>= a1 && b2 <= a2) {
+                Part1++;
+            }
+
+            if (a2 >= b1 && b2 >= a1) {
+                Part2++;
+            }
+        }
+    }
+}
